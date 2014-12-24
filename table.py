7036e22d67734597091638ba88916ade10ca4161
Ilya Margolin <ilya@jimdo.com>
2014-12-24 18:18:30 +0100

    asked a question in pyo-discuss

--- table.py
+++ table.py
@@ -1,7 +1,27 @@
+beats = [(0, 1), (0.5, 1), (1.5, 2), (2.0, 1)]
+whole_length = 2.5
+
 from pyo64 import * # noqa
-
-snd_path = ('/srv/media/samples/musicradar-metal-drum-samples/drums acoustic/'
-            '058bpm_drums_acoustic/058full001.wav')
-
 server = Server().boot().start()
-sndt = SndTable(snd_path, stop=0.77)
+
+trigger_table = NewTable(whole_length)
+for pos, vol in beats:
+    trigger_table.put(
+        1.0,
+        # convert seconds to samples
+        pos=int(server.getSamplingRate() * pos)
+    )
+trigger_signal = TableRead(
+    trigger_table,
+    freq=server.getSamplingRate(),
+    interp=1,  # no interpolation, so ones stay ones
+    loop=1
+).play()
+test_signal = Noise()
+env_table = HannTable()
+trigger_env = TrigEnv(trigger_signal, env_table, dur=0.1)
+test_signal.mul = trigger_env
+test_signal.out()
+
+snd_path = '058full001.wav'
+sndt = SndTable(snd_path, stop=0.757)
